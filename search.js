cd78236e17db08aeecb97e661521b7c01fbb6971
Nyamao Gregory Kaiser$ <nyamaokaiser79@gmail.com>
2023-06-30 01:24:21 +0300

    Openai

---
+++ search.js
@@ -0,0 +1,27 @@
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+const searchResults = document.getElementById('output-box');
+
+searchButton.addEventListener("click", async () => {
+  const input = searchInput.value;
+  
+  const response = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 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' // Replace YOUR_API_KEY with your actual OpenAI API key
+    },
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'system', content: 'I am Gregory. Your AI chatbot' },
+
+        //{ role: 'system', content: 'You are Gregory.An AI chatbot' }, // Set the system message to introduce Gregory
+        { role: 'user', content: input }
+      ]
+    })
+  });
+
+  const data = await response.json();
+  const result = data.choices[0].message.content;
+  searchResults.textContent = result;
+});
